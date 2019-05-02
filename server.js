33ef9753aab1de17b1f174de0461ee1b042f9c83
Lukasz Piotrowski <lpiotrowski@op.pl>
2019-05-02 23:02:34 +0200

    Zadanie_25_3 Finished

---
+++ server.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var app = express();
+
+
+//var server = app.listen(3000, function() {
+//    console.log('Przykładowa aplikacja nasłuchuje na http://localhost:3000');
+//});
+
+app.listen(3000);
+
+//app.get('/:id', function (req, res) {
+//    res.send('Identyfikator, który został dopisany to ' + req.params.id);
+//});
+
+app.get('/', function (req, res) {
+    console.log('Otrzymałem żądanie GET do strony głównej');
+    res.send('Hello GET!');
+});
+
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+});
+
+app.post('/', function (req, res) {
+    console.log('Otrzymałem żądanie POST do strony głównej');
+    res.send('Hello POST!');
+});
+
+app.delete('/del_user', function (req, res) {
+    console.log('Otrzymałem żądanie DELETE do strony /del_user');
+    res.send('Hello DELETE!');
+});
+
+app.get('/list_user', function (req, res) {
+    console.log('Otrzymałem żądanie GET do strony /list_user');
+    res.send('Strona z listą użytkowników!');
+});
+
+app.get('/ab*cd', function(req, res) {
+    console.log('Otrzymałem żądanie GET do strony /ab*cd');
+    res.send('Wzór pasuje');
+});
