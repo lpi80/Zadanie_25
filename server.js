7896d4332e788b1eee22efe29ed9cb995d7ecee8
Lukasz Piotrowski <lpiotrowski@op.pl>
2019-05-03 00:46:10 +0200

    Zadanie 1

--- server.js
+++ server.js
@@ -1,21 +1,13 @@
 var fs = require('fs')
-
 var express = require('express');
 var app = express();
-
-app.get('/', function (req, res) {
-    res.send('Hello world');
-});
-
-
-
 var stringifyFile;
+var bodyParser = require('body-parser');
+
 app.use(bodyParser.json());
 
-
 app.get('/getNote', function (req, res) {
-    res.send('Odczytujemy');
-    fs.readFile('./test.json', 'utf8', function(err, data) {
+    fs.readFile('test.json', 'utf8', function(err, data) {
         if (err) throw err;
         stringifyFile = data
         res.send(data);
@@ -23,8 +15,8 @@ app.get('/getNote', function (req, res) {
 });
 
 app.post('/updateNote/:note', function (req, res) {
-    res.send('Zapisujemy');
-    fs.writeFile('./test.json', stringifyFile, function(err) {
+    stringifyFile = stringifyFile + req.params.note;
+    fs.writeFile('test.json', stringifyFile, function(err) {
         if (err) throw err;
         console.log('file updated');
     });
