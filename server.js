10d947cde2ebbef8c4ec47c2bfe42b13ec15c75d
Lukasz Piotrowski <lpiotrowski@op.pl>
2019-05-05 11:29:51 +0200

    Zadanie_25_6 Finished

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views','./views');
+
+app.get('/', function (req, res) {
+    res.render('main', {
+        url: "/auth/google"
+    });
+});
+
+app.get('/auth/google', function (req, res) {
+    res.render('logowanie');
+});
+
+app.get('/userPanel', function (req, res) {
+    const response = {
+        first_name: req.query.first_name,
+        last_name: req.query.last_name
+    };
+    res.json(response)
+});
+
+app.listen(3000);
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+});
+
