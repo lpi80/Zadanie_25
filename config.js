6837ba70295c258541a7a0987db7a1ff0ca2e2e2
Lukasz Piotrowski <lpiotrowski@op.pl>
2019-05-08 13:17:15 +0200

    Finished

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '579739451707-8rt44cqb3r6l7q37ai7kiu0q1e5lbkae.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'tIdnHGzn0f9y0QHRp65xTeq3',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
